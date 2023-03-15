b64a0a4c74a6ec6561e35bb213107d69f049f7b4
FredoSalat <fredrik.marthinsens@gmail.com>
2023-03-15 21:36:22 +0100

    MUI navbar and table added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import CountriesList from "./components/pages/CountriesList";
 import "./App.css";
-import Layout from "./components/layout/Layout";
 import Index from "./routes";
 
 function App() {
